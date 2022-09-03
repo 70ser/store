2d06aac352b40334820609d5616c0e8ef6205867
RB <biasedclover@gmail.com>
2022-09-03 13:26:25 +0800

    Failure

--- src/main/java/com/sp/store/entity/Order.java
+++ src/main/java/com/sp/store/entity/Order.java
@@ -3,6 +3,7 @@ package com.sp.store.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
@@ -18,7 +19,9 @@ public class Order {
     private Integer userId;
     private Integer bookId;
     private String alipayNo;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date payTime;
     private Short state;
     private BigDecimal total;
--- src/main/java/com/sp/store/entity/User.java
+++ src/main/java/com/sp/store/entity/User.java
@@ -12,6 +12,7 @@ public class User {
     private String userName;
     private String password;
     private String nickName;
+    private String email;
     private String phoneNumber;
     private String address;
     @TableLogic
